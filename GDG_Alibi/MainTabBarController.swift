6a418df6efdb1b89b45f348a7468b34166f1a1d6
emily <emily@kmong.com>
2021-07-05 23:50:36 +0900

    탭바 구성

--- GDG_Alibi/MainTabBarController.swift
+++ GDG_Alibi/MainTabBarController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainTabBarController.swift
 //  GDG_Alibi
 //
 //  Created by chungeunji on 2021/07/05.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        tabBar.tintColor = .green
     }
 
-
 }
-
