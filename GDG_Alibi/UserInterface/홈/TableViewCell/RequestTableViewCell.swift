9d714d07e1201ca41b0f5b7dc2dd5f9614a205c6
eunji <twih1203@gmail.com>
2021-07-08 12:10:48 +0900

    요청 셀 추가

--- GDG_Alibi/UserInterface/홈/TableViewCell/RequestTableViewCell.swift
+++ GDG_Alibi/UserInterface/홈/TableViewCell/RequestTableViewCell.swift
@@ -7,17 +7,12 @@
 
 import UIKit
 
-class RequestTableViewCell: UITableViewCell {
+final class RequestTableViewCell: UITableViewCell {
+
+    static let id = "RequestTableViewCell"
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
 }
