1fbb8e8647bb75b36d5541a68064c7a90d76cac6
eunji <twih1203@gmail.com>
2021-07-09 00:45:02 +0900

    요청 폼에 들어가는 tableview cell 추가

---
+++ GDG_Alibi/UserInterface/요청/TableViewCell/CategoryTableViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  CategoryTableViewCell.swift
+//  GDG_Alibi
+//
+//  Created by chungeunji on 2021/07/08.
+//
+
+import UIKit
+
+final class CategoryTableViewCell: UITableViewCell {
+
+    static let id = "CategoryTableViewCell"
+
+    @IBOutlet var boxViews: [UIView]!
+    @IBOutlet var checkImageViews: [UIImageView]!
+    @IBOutlet var categoryLabels: [UILabel]!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        initView()
+    }
+
+    private func initView() {
+        boxViews.forEach({
+            $0.layer.borderWidth = 1
+            $0.layer.borderColor = #colorLiteral(red: 0.8, green: 0.8, blue: 0.8, alpha: 1)
+            $0.layer.cornerRadius = 17
+        })
+    }
+
+    @IBAction func selectCategory(_ sender: UIButton) {
+        let tag = sender.tag
+        checkImageViews.forEach({ $0.isHidden = $0.tag != tag })
+        boxViews.forEach({
+            $0.layer.borderColor = $0.tag == tag ? #colorLiteral(red: 0.1725490196, green: 0.7803921569, blue: 0.5058823529, alpha: 1) : #colorLiteral(red: 0.8, green: 0.8, blue: 0.8, alpha: 1)
+            $0.backgroundColor = $0.tag == tag ? #colorLiteral(red: 0.1725490196, green: 0.7803921569, blue: 0.5058823529, alpha: 1) : .systemBackground
+        })
+        categoryLabels.forEach({ $0.textColor = $0.tag == tag ? .white :  #colorLiteral(red: 0.6666666667, green: 0.6666666667, blue: 0.6666666667, alpha: 1) })
+    }
+
+}
---
+++ GDG_Alibi/UserInterface/요청/TableViewCell/DueDateTableViewCell.swift
@@ -0,0 +1,64 @@
+//
+//  DueDateTableViewCell.swift
+//  GDG_Alibi
+//
+//  Created by chungeunji on 2021/07/08.
+//
+
+import UIKit
+
+final class DueDateTableViewCell: UITableViewCell {
+
+    static let id = "DueDateTableViewCell"
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+
+    @IBOutlet var chevronImageViews: [UIImageView]!
+    @IBOutlet var boxViews: [UIView]!
+
+    @IBOutlet weak var pickerHeightConstraint: NSLayoutConstraint!
+
+    @IBOutlet weak var pickerView: UIDatePicker!
+
+    var didSelectDateBox: (() -> Void)?
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        initView()
+    }
+
+    private func initView() {
+        boxViews.forEach({
+            $0.layer.cornerRadius = 17
+            $0.layer.borderColor = #colorLiteral(red: 0.1529411765, green: 0.231372549, blue: 0.2823529412, alpha: 1)
+            $0.layer.borderWidth = 1
+        })
+
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM월dd일"
+        let today = dateFormatter.string(from: Date())
+        dateLabel.text = today
+        dateFormatter.dateFormat = "HH시mm분"
+        let currentTime = dateFormatter.string(from: Date())
+        timeLabel.text = currentTime
+    }
+
+    @IBAction func selectBox(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        let isSelected = sender.isSelected
+        pickerView.isHidden = !sender.isSelected
+        pickerHeightConstraint.constant = isSelected ? 216 : 0
+        chevronImageViews[sender.tag].transform = isSelected ? CGAffineTransform(rotationAngle: .pi) : .identity
+
+        UIView.animate(withDuration: 0.1) {
+            self.layoutIfNeeded()
+        } completion: { _ in
+            self.didSelectDateBox?()
+        }
+    }
+
+    @IBAction func selectDate(_ sender: UIDatePicker) {
+    }
+}
