29915a776b624e939c9928733586f2008fa87926
eunji <twih1203@gmail.com>
2021-07-10 13:36:35 +0900

    인트로 뷰 추가

---
+++ GDG_Alibi/UserInterface/IntroViewController.swift
@@ -0,0 +1,50 @@
+//
+//  IntroViewController.swift
+//  GDG_Alibi
+//
+//  Created by chungeunji on 2021/07/09.
+//
+
+import UIKit
+
+final class IntroViewController: UIViewController, ViewInfo {
+
+    static var id = "IntroViewController"
+
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        initView()
+        fetchLocation()
+    }
+
+    private func initView() {
+        activityIndicator.startAnimating()
+    }
+
+    private func fetchLocation() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.25, execute: {
+            LocationManager.shared.fetchLocationList { [weak self] in
+                self?.showMainVC()
+            }
+        })
+    }
+
+    private func showMainVC() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+
+        if LocationManager.shared.isLocationEmpty {
+            let storyBoard = UIStoryboard(name: "Location", bundle: nil)
+            guard let vc = storyBoard.instantiateInitialViewController() as? UINavigationController else { return }
+            appDelegate.window?.rootViewController = vc
+            appDelegate.window?.makeKeyAndVisible()
+        } else {
+            let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+            guard let vc = storyBoard.instantiateViewController(withIdentifier: MainTabBarController.id) as? MainTabBarController else { return }
+            appDelegate.window?.rootViewController = vc
+            appDelegate.window?.makeKeyAndVisible()
+        }
+    }
+}
