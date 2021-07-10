b7fd76311203ad2f5172838e25fe1b2a523708e3
eunji <twih1203@gmail.com>
2021-07-10 14:38:07 +0900

    프로필 뷰 컨트롤러 UI 구성

---
+++ GDG_Alibi/BasicUserInfo.swift
@@ -0,0 +1,8 @@
+//
+//  BasicUserInfo.swift
+//  GDG_Alibi
+//
+//  Created by chungeunji on 2021/07/10.
+//
+
+import Foundation
---
+++ GDG_Alibi/UserInterface/마이/ProfileViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileViewModel.swift
+//  GDG_Alibi
+//
+//  Created by chungeunji on 2021/07/10.
+//
+
+import Foundation
+
+class ProfileViewModel {
+    
+}
